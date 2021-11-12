0fbf8b75ffecac92306655e4a6126b4d6c1ca227
Ervīns Krastiņš <69342901+KrastinsE@users.noreply.github.com>
2021-11-12 05:24:12 +0200

    Update dbcnt.php

--- upes/dbcnt.php
+++ upes/dbcnt.php
@@ -1,5 +1,5 @@
-<?php
-$conn=mysqli_connect("sql209.epizy.com","epiz_29373619","fymmWUI7vWJx7r","epiz_29373619_upes_db");
+<?php 
+$conn=mysqli_connect("localhost","root","","upes_db");
 if(!$conn){
 	die("Pieslēguma kļūda: ".mysqli_connect_error());
 }
